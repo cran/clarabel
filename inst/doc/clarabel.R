af94547beb7cb0b508dfff19ee1720e5885f94fd
Balasubramanian Narasimhan <naras@stanford.edu>
2024-06-22 17:40:02 +0000

    version 0.9.0

--- inst/doc/clarabel.R
+++ inst/doc/clarabel.R
@@ -1,4 +1,4 @@
-## ---- include = FALSE---------------------------------------------------------
+## ----include = FALSE----------------------------------------------------------
 knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>"
@@ -58,7 +58,7 @@ mat <- function(v) {
   S
 }
 
-## ---- echo = TRUE-------------------------------------------------------------
+## ----echo = TRUE--------------------------------------------------------------
 q <- c(1, -1, 1) # objective: x_1 - x2 + x_3
 A11 <- matrix(c(-7, -11, -11, 3), nrow = 2)
 A12 <- matrix(c(7, -18, -18, 8), nrow = 2)
@@ -77,10 +77,36 @@ A <- rbind(
 )
 b <- c(vec(B1), vec(B2)) # stack both psd constraints
 cones <- list(s = c(2, 3)) # cone dimensions
-s <- clarabel(A = A, b = b, q = q, cone = cones)
+s <- clarabel(A = A, b = b, q = q, cones = cones)
 cat(sprintf("Solution status, description: = (%d, %s)\n",
             s$status, solver_status_descriptions()[s$status]))
 cat(sprintf("Solution (x1, x2, x3) = (%f, %f, %f)\n", s$x[1], s$x[2], s$x[3]))
+
+## ----echo = FALSE-------------------------------------------------------------
+parameter_df <- data.frame(
+  Parameter = c("z", "l", "q", "s", "ep", "p", "gp"),
+  Type = c("integer", "integer", "integer", "integer", "integer", "numeric", "list"),
+  Length = c("1", "1", ">= 1", ">= 1", "1", ">= 1", ">= 1"),
+  Description = c(
+    "Number of primal zero cones (dual free cones), which corresponds to the primal equality constraints",
+    "Number of linear cones (non-negative cones)",
+    "Vector of second-order cone sizes",
+    "Vector of positive semidefinite cone sizes",
+    "Number of primal exponential cones",
+    "Vector of primal power cone parameters",
+    "List of named lists of two items, `a` : the numeric vector of at least 2 exponent terms, and `n` : an integer dimension of generalized power cone parameters"
+  ),
+  Definition = c("$\\{ 0 \\}^{z}$",
+                 "$\\{ x \\in \\mathbb{R}^{l} : x_i \\ge 0, \\forall i=1,\\dots,l \\}$",
+                 "$\\{ (t,x) \\in \\mathbb{R}^{q}  :  \\lVert x\\rVert_2  \\leq t \\}$",
+                 "Upper triangular part of the positive semidefinite cone $S^s_+$. The elements $x$ of this cone represent the columnwise stacking of the upper triangular part of a positive semidefinite matrix $X \\in S^s_+$, so that $x \\in R^d$ with $d = s(s+1)/2$",
+  "$\\{(x, y, z) : y > 0,~~ ye^{x/y} \\le z \\}$",
+  "$\\{(x, y, z) : x^p y^{(1-p)} \\ge  \\lVert z\\rVert,~ (x,y) \\ge 0 \\}$ with $p \\in (0,1)$",
+  "$\\{(x, y) \\in R^{len(a)} \\times R^n : \\prod\\limits_{a_i \\in a} x_i^{a_i} \\ge \\lVert y\\rVert_2,~ x \\ge 0 \\}$ with $a_i \\in (0,1)$ and $\\sum a_i = 1$"
+  )
+)
+names(parameter_df)[5] <- "Definition (per parameter element)"
+knitr::kable(parameter_df)
 
 ## -----------------------------------------------------------------------------
 P <- Matrix::Matrix(2 * c(0, 0, 0, 1), nrow = 2, ncol = 2, sparse = TRUE)
